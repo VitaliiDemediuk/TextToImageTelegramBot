164d0d918d2034842d446e396524511aa276c7c9
Vitalii Demediuk <vitaliidemediuk@Vitaliis-MacBook-Air.local>
2023-04-23 13:13:18 +0300

    variation command handling

--- bot.py
+++ bot.py
@@ -88,11 +88,64 @@ def process_image(message: Message, prompt: str):
         bot.reply_to(message, "Sorry, something went wrong. Please try again.")
 
 
+# Handler function to generate image variations
+@bot.message_handler(commands=['variation'])
+def variation_handler(message: Message):
+    bot.reply_to(message, "Please upload an image.")
+    bot.register_next_step_handler(message, process_variation)
+
+
+# Function to process uploaded image and generate image variations
+def process_variation(message: Message):
+    try:
+        file_id = message.photo[-1].file_id
+        file_info = bot.get_file(file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+        # Create Image object from uploaded file
+        jpg_img = Image.open(io.BytesIO(downloaded_file))
+        img_size = jpg_img.size
+        rgba_img = jpg_img.convert('RGBA')
+        png_io = io.BytesIO()
+        rgba_img.save(png_io, format='PNG')
+        png_bytes = png_io.getvalue()
+
+        # Ask user for variation count
+        bot.reply_to(message, "How many variations do you want?")
+        bot.register_next_step_handler(message, process_variation_count, png_bytes, img_size)
+    except:
+        bot.reply_to(message, "Sorry, something went wrong. Please try again.")
+
+
+# Function to process variation count and generate variations
+def process_variation_count(message: Message, png_bytes: bytes, img_size: tuple):
+    try:
+        variation_count = int(message.text.strip())
+
+        # Generate image variations using OpenAI API
+        response = openai.Image.create_variation(
+            image=png_bytes,
+            n=variation_count,
+            size="{}x{}".format(img_size[0], img_size[1])
+        )
+
+        # Send image variations to user
+        for variation in response['data']:
+            bot.send_photo(message.chat.id, variation['url'])
+
+    except ValueError:
+        bot.reply_to(message, "Please enter a valid number.")
+        bot.register_next_step_handler(message, process_variation_count, png_bytes, img_size)
+    except:
+        bot.reply_to(message, "Sorry, something went wrong. Please try again.")
+
+
 def main():
     command_list = []
     command_list.append(BotCommand('/start', 'Displays a welcome message.'))
     command_list.append(BotCommand('/generate_image', 'Generates an image based on the prompt you provide. For example, "/generate_image red apple".'))
     command_list.append(BotCommand('/edit_image', 'Upload an image and apply filters to it.'))
+    command_list.append(BotCommand('/variation', 'Upload an image and apply variations to it.'))
 
     # Set bot commands
     bot.set_my_commands(command_list)
